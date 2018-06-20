62d1d02a1694da44f0524f1cd21edbfb15edd8cd
Vladimir Nadygin <vnadygin@me.com>
2018-06-20 18:23:31 +0300

    1.1.2: Minor patch

--- demo/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Dmitriy Bizyaev
- */
-
-'use strict';
-
-export const GITLAB_API_URL = 'https://gitlab.ordbuy.com/api/v3';
-export const GITLAB_API_ISSUES_PATH = 'projects/83/issues';
-export const GITLAB_API_PRIVATE_TOKEN = 'yN9by_ykyFxAcongwxB6';
