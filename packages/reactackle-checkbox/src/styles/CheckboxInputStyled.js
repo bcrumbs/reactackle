85937e4e502111f14c8ab5260e2419447ddb1b26
Katya Marova <marova.ekaterina@gmail.com>
2018-06-20 20:22:45 +0300

    Bug fixed: Double focus on checkbox [https://github.com/bcrumbs/reactackle/issues/101](#101)

--- packages/reactackle-checkbox/src/styles/CheckboxInputStyled.js
+++ packages/reactackle-checkbox/src/styles/CheckboxInputStyled.js
@@ -1,10 +1,9 @@
-'use strict';
-
 import styled from 'styled-components';
 import { elementInvisible } from 'reactackle-core';
 
 export const CheckboxInputStyled = styled.input`
-  ${/* Prettier workarount until issue is not fixed: https://github.com/prettier/prettier/issues/2291 */ ''};
+  ${/* Prettier workaround until issue is not fixed:
+   https://github.com/prettier/prettier/issues/2291 */ ''};
   ${elementInvisible};
 `;
 
